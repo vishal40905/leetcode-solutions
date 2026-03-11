cb9c8414682bd37d2f0ab0dae59743b260a6b4c8
Vishal Jadhav <107361957+vishal40905@users.noreply.github.com>
2026-03-11 11:23:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -3,7 +3,7 @@ class Solution:
         max=0
         start = prices[0]
         len1 = len(prices)
-        for i in range (0,len1):
+        for i in range (1,len1):
             if start < prices[i]:
                 max+=prices[i]-start
             start = prices[i]
